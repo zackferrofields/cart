2cd51af9eaff08e49abb6a2f0a60d5b5270b0f71
Zack Ferro-Fields <zackferrofields@gmail.com>
2017-01-14 13:23:08 +0000

    list items

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const { Future } = require('ramda-fantasy');
+const {
+  compose, slice, split, map, flatten, reject, isEmpty, toLower,
+  reduceBy, inc, identity, uncurryN
+} = require('ramda');
+
+const reduceItems = reduceBy(inc, 0, identity);
+
+const offer = uncurryN(2, x => y => y - (Math.floor(y / x)));
+
+const parseList = compose(reject(isEmpty), flatten, map(compose(split(','), toLower)));
+
+const list = Future.of(slice(2, Infinity, process.argv))
+  .map(parseList)
+  .map(reduceItems);
+
+list
+  .fork(console.error, console.log);
