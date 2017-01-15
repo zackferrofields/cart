51e27506fc3ba1b0948321ed5f54f753d12b0b09
Zack Ferro-Fields <zackferrofields@gmail.com>
2017-01-15 22:44:01 +0000

    print tidy

--- src/print.js
+++ src/print.js
@@ -1,23 +1,30 @@
 const { IO } = require('ramda-fantasy');
 const {
   add, always, compose, equals, forEachObjIndexed,
-  multiply, reduce, tap, toPairs, unless
+  multiply, reduce, tap, toPairs, unless, divide
 } = require('ramda');
+
+const currencyLocale = 'en-GB';
+
+const currenctFormat = { style: 'currency', currency: 'GBP' };
 
 const printLn = data => process.stdout.write(`${data}\n`);
 
+const toPrice = x => divide(x, 100).toLocaleString(currencyLocale, currenctFormat);
+
 const printItem = ([{price = 0, quantity = 0}, name]) =>
-  printLn(`${name} ${multiply(quantity, price)}`);
+  printLn(`${name} ${toPrice(multiply(quantity, price))}`);
 
 const printOffer = ([{price = 0, quantity = 0, offer = always(0)}]) =>
-  unless(equals(0), printLn)(offer(quantity, price));
+  unless(equals(0), compose(printLn, toPrice))(offer(quantity, price));
 
-const printTotal = compose(
+const printTotal = tap(compose(
   printLn,
   tap(() => printLn('-----')),
+  toPrice,
   reduce((acc, [, {price = 0, quantity = 0, offer = always(0)}]) =>
     add(acc, add(offer(quantity, price), multiply(quantity, price))), 0),
-  toPairs);
+  toPairs));
 
 const printItems = tap(forEachObjIndexed((...args) =>
   compose(tap(printOffer), tap(printItem))(args)));
