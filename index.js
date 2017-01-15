a62f2cf8c52d52351acc1d8d363e1306a73a4dd1
Zack Ferro-Fields <zackferrofields@gmail.com>
2017-01-15 12:39:43 +0000

    print

--- index.js
+++ index.js
@@ -2,18 +2,40 @@ const { IO } = require('ramda-fantasy');
 const {
   compose, slice, split, map, flatten, reject, isEmpty, toLower, __, multiply,
   reduceBy, inc, identity, uncurryN, reduce, toPairs, merge, over, lensProp,
+  forEachObjIndexed, tap, add, always, unless, equals
 } = require('ramda');
 
 const items = {
   apple: { price: 25 },
   orange: { price: 30 },
   garlic: { price: 15 },
-  papaya: { price: 50, offer: 3 }
+  papaya: { price: 50 }
 };
+
+const offerBuyX = uncurryN(3, x => y => z => (x ? z * - (Math.floor(y / x)) : 0));
+
+items.papaya.offer = offerBuyX(3);
 
 const argsIO = IO(() => slice(2, Infinity, process.argv));
 
-const logIO = data => IO(() => console.log(data));
+const printItem = ([{price = 0, quantity = 0}, name]) =>
+  process.stdout.write(`${name} ${multiply(quantity, price)}\n`);
+
+const printOffer = ([{price = 0, quantity = 0, offer = always(0)}]) =>
+  unless(equals(0), amount => process.stdout.write(`${amount}\n`))(offer(quantity, price));
+
+const printTotal = compose(
+  total => process.stdout.write(`-----\n${total}\n`),
+  reduce((acc, [, {price = 0, quantity = 0, offer = always(0)}]) =>
+    add(acc, add(offer(quantity, price), multiply(quantity, price))), 0),
+  toPairs);
+
+const printItems = forEachObjIndexed((...args) =>
+  compose(tap(printOffer), tap(printItem))(args));
+
+const printIO = data => IO(() => printItems(data) && printTotal(data));
+
+const parseList = compose(reject(isEmpty), flatten, map(compose(split(','), toLower)));
 
 const countItems = reduceBy(inc, 0, identity);
 
@@ -23,13 +45,9 @@ const mergeItems = uncurryN(3, name => quantity =>
 const reduceItems = reduce((acc, [name, quantity]) =>
   mergeItems(name, quantity, acc), items);
 
-const offer = uncurryN(2, x => y => y - (Math.floor(y / x)));
-
-const parseList = compose(reject(isEmpty), flatten, map(compose(split(','), toLower)));
-
 const main = argsIO
   .map(compose(countItems, parseList))
   .map(compose(reduceItems, toPairs))
-  .chain(logIO);
+  .chain(printIO);
 
 main.runIO();
