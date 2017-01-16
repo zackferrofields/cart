9866b4c82a9d06d7d941cf8f9ae6affd5b350ef3
Zack Ferro-Fields <zackferrofields@gmail.com>
2017-01-16 15:45:38 +0000

    Hindley–Milner types

--- src/items/index.js
+++ src/items/index.js
@@ -2,6 +2,7 @@ const {
   __, compose, filter, flatten, identity, inc, isEmpty, lensProp, lt, map, merge,
   over, propOr, reduce, reduceBy, reject, split, toLower, toPairs, uncurryN
 } = require('ramda');
+const { BxGOF } = require('../offers');
 
 const items = {
   apple: { price: 25 },
@@ -10,22 +11,27 @@ const items = {
   papaya: { price: 50 }
 };
 
-const offerBuyX = uncurryN(3, x => y => z => (x ? z * - (Math.floor(y / x)) : 0));
+// :: Number -> Number -> Number
+items.papaya.offer = BxGOF(3);
 
-items.papaya.offer = offerBuyX(3);
-
+// :: [[String]] -> [String]
 const parseList = compose(reject(isEmpty), flatten, map(compose(split(','), toLower)));
 
+// :: [String] -> Object
 const countItems = reduceBy(inc, 0, identity);
 
+// :: String -> Number -> Object -> Object
 const mergeItems = uncurryN(3, name => quantity =>
   over(lensProp(name), merge(__, { quantity })));
 
+// :: Object -> Object
 const reduceItems = reduce((acc, [name, quantity]) =>
   mergeItems(name, quantity, acc), items);
 
+// :: Object -> Object
 const filterItems = filter(compose(lt(0), propOr(0, 'quantity')));
 
+// :: [[String]] -> Object
 const listItems = compose(filterItems, reduceItems, toPairs, countItems, parseList);
 
 module.exports = listItems;
